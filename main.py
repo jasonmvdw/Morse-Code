084c5bb1fbd57aaf6815ae838c47e899de85f154
jasonmvdw <jasonvdw1012@gmail.com>
2024-02-29 14:03:47 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+morse_codes = {
+    "a" : "•⁃",
+    "b" : "⁃•••",
+    "c" : "⁃•⁃•",
+    "d" : "⁃••",
+    "e" : "•",
+    "f" : "••⁃•",
+    "g" : "⁃⁃•",
+    "h" : "••••",
+    "i" : "••",
+    "j" : "•⁃⁃⁃",
+    "k" : "⁃•⁃",
+    "l" : "•⁃••",
+    "m" : "⁃⁃",
+    "n" : "⁃•",
+    "o" : "⁃⁃⁃",
+    "p" : "•⁃⁃•",
+    "q" : "⁃⁃•⁃",
+    "r" : "•⁃•",
+    "s" : "•••",
+    "t" : "⁃",
+    "u" : "••⁃",
+    "v" : "•••⁃",
+    "w" : "•⁃⁃",
+    "x" : "⁃••⁃",
+    "y" : "⁃•⁃⁃",
+    "z" : "⁃⁃••",
+    "1" : "•⁃⁃⁃⁃",
+    "2" : "••⁃⁃⁃",
+    "3" : "•••⁃⁃",
+    "4" : "••••⁃",
+    "5" : "••••",
+    "6" : "⁃••••",
+    "7" : "⁃⁃•••",
+    "8" : "⁃⁃⁃••",
+    "9" : "⁃⁃⁃⁃•",
+    "0" : "⁃⁃⁃⁃⁃",   
+}
+
+text_to_convert = input("What would you like to convert to morse code? : \n").lower()
+
+converted_text = ''
+for letter in text_to_convert:
+    if letter in morse_codes:
+        morse_letter = morse_codes[letter]
+        converted_text += morse_letter + ", "
+    else:
+        print(f"{letter} is an Invalid Character")
+            
+print(converted_text)
+    
+
+  
+  
+  
+
+
